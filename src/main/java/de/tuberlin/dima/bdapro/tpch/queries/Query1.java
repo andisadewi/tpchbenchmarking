41271fa3768c1d7d37d5c343bf9898dbb22a86a5
Andisa Dewi <andisa.dewi@icloud.com>
2017-05-17 13:29:26 +0200

    added flink to pom

--- src/main/java/de/tuberlin/dima/bdapro/tpch/queries/Query1.java
+++ src/main/java/de/tuberlin/dima/bdapro/tpch/queries/Query1.java
@@ -1,8 +1,13 @@
 package de.tuberlin.dima.bdapro.tpch.queries;
 
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Random;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.io.CsvReader;
-import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.tuple.Tuple7;
 
 import de.tuberlin.dima.bdapro.tpch.PathConfig;
@@ -16,20 +21,40 @@ public class Query1 {
 		this.env = env;
 		this.sf = sf;
 
+		final int delta = getDelta();
+		final LocalDate thresholdDate = LocalDate.of(1998, 12, 1);
+
+		DataSet<Tuple7<Integer, Double, Double, Double, String, String, LocalDate>> lineitems = readLineitem();
+
+		lineitems.filter(new FilterFunction<Tuple7<Integer, Double, Double, Double, String, String, LocalDate>>() {
+
+			@Override
+			public boolean filter(Tuple7<Integer, Double, Double, Double, String, String, LocalDate> arg0)
+					throws Exception {
+				return false;
+			}
+		});
+
 	}
 
-	private DataSource<Tuple7<Integer, Double, Double, Double, String, String, String>> readLineitem() {
+	private DataSet<Tuple7<Integer, Double, Double, Double, String, String, LocalDate>> readLineitem() {
 		CsvReader source = null;
-		switch(sf){
-		case("1.0"):
+		switch (sf) {
+		case ("1.0"):
+			source = env.readCsvFile(PathConfig.LINEITEM_1);
+			break;
+		default:
 			source = env.readCsvFile(PathConfig.LINEITEM_1);
 			break;
 		}
-		
-		return source.fieldDelimiter("|")
-		.includeFields("0000111111100000")
-		.types(Integer.class, Double.class, Double.class, Double.class, String.class, String.class, String.class);
-		
+
+		return source.fieldDelimiter("|").includeFields("0000111111100000").types(Integer.class, Double.class,
+				Double.class, Double.class, String.class, String.class, LocalDate.class);
+	}
+
+	private int getDelta() {
+		Random rand = new Random();
+		return 60 + rand.nextInt((120 - 60) + 1);
 	}
 
 }
