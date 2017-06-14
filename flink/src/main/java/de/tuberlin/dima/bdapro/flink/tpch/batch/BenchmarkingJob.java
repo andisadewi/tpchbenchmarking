09bbb13ae26255b8a358ee4823a174915f4864d6
Hekmatullah Sajid <hekmatullah.sajid@campus.tu-berlin.de>
2017-06-14 02:47:55 +0200

    Benchmarking Flink batch

--- flink/src/main/java/de/tuberlin/dima/bdapro/flink/tpch/batch/BenchmarkingJob.java
+++ flink/src/main/java/de/tuberlin/dima/bdapro/flink/tpch/batch/BenchmarkingJob.java
@@ -11,7 +11,7 @@ import org.apache.flink.table.api.java.BatchTableEnvironment;
 
 import de.tuberlin.dima.bdapro.flink.tpch.TableSourceProvider;
 import de.tuberlin.dima.bdapro.flink.tpch.batch.queries.Query;
-import de.tuberlin.dima.bdapro.flink.tpch.batch.queries.Query2;
+import de.tuberlin.dima.bdapro.flink.tpch.batch.queries.*;
 
 /**
  * How to use: ./bin/flink run benchmarkingJob.jar <path//to//the//testDB>
@@ -53,18 +53,145 @@ public class BenchmarkingJob {
 		provider.setBaseDir(path);
 		BatchTableEnvironment tableEnv = provider.loadDataBatch(env, sf);
 
-		List<Long> results = new ArrayList<Long>();
+		List<String> results = new ArrayList<String>();
+		
 		long start = System.currentTimeMillis();
-		final Query q13 = new Query2(tableEnv);
-		q13.execute();
+		//final Query q1 = new Query1(tableEnv);
+		//q2.execute();
 		long end = System.currentTimeMillis();
-		results.add(end - start);
+		//results.add(" Query1 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		start = System.currentTimeMillis();
+		//final Query q2 = new Query2(tableEnv);
+		//q2.execute();
+		end = System.currentTimeMillis();
+		//results.add(" Query2 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		start = System.currentTimeMillis();
+		final Query q3 = new Query3(tableEnv);
+		q3.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query3 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		 start = System.currentTimeMillis();
+		final Query q4 = new Query4(tableEnv);
+		q4.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query4 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		start = System.currentTimeMillis();
+		final Query q5 = new Query5(tableEnv);
+		q5.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query5 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+//		start = System.currentTimeMillis();
+//		final Query q6 = new Query6(tableEnv);
+//		q6.execute();
+//		end = System.currentTimeMillis();
+//		results.add(" Query6 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		 start = System.currentTimeMillis();
+		final Query q7 = new Query7(tableEnv);
+		q7.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query7 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		start = System.currentTimeMillis();
+		final Query q8 = new Query8(tableEnv);
+		q8.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query8 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		start = System.currentTimeMillis();
+		final Query q9 = new Query9(tableEnv);
+		q9.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query9 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		 start = System.currentTimeMillis();
+		final Query q10 = new Query10(tableEnv);
+		q10.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query10 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		start = System.currentTimeMillis();
+		final Query q11 = new Query11(tableEnv, "1.0");
+		q11.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query11 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		start = System.currentTimeMillis();
+		final Query q12 = new Query12(tableEnv);
+		q12.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query12 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		 start = System.currentTimeMillis();
+		final Query q13 = new Query13(tableEnv);
+		q13.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query13 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		start = System.currentTimeMillis();
+		final Query q14 = new Query14(tableEnv);
+		q14.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query14 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		start = System.currentTimeMillis();
+		final Query q15 = new Query15(tableEnv);
+		q15.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query15 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+//		start = System.currentTimeMillis();
+//		final Query q16 = new Query16(tableEnv);
+//		q16.execute();
+//		end = System.currentTimeMillis();
+//		results.add(" Query16 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+//		start = System.currentTimeMillis();
+//		final Query q17 = new Query17(tableEnv);
+//		q17.execute();
+//		end = System.currentTimeMillis();
+//		results.add(" Query17 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		start = System.currentTimeMillis();
+		final Query q18 = new Query18(tableEnv);
+		q18.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query18 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		 start = System.currentTimeMillis();
+		final Query q19 = new Query19(tableEnv);
+		q19.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query19 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+		start = System.currentTimeMillis();
+		final Query q20 = new Query20(tableEnv);
+		q20.execute();
+		end = System.currentTimeMillis();
+		results.add(" Query20 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+//		start = System.currentTimeMillis();
+//		final Query q21 = new Query21(tableEnv);
+//		q21.execute();
+//		end = System.currentTimeMillis();
+//		results.add(" Query21 execution time in miliseconds: " + (end - start) + "\r\n");
+		
+//		 start = System.currentTimeMillis();
+//		final Query q22 = new Query22(tableEnv);
+//		q22.execute();
+//		end = System.currentTimeMillis();
+//		results.add(" Query22 execution time in miliseconds: " + (end - start) + "\r\n");
 
 		////////////////////////WRITE OUTPUT TO FILE ///////////////////////////////
 		try {
 			FileWriter writer = new FileWriter("FlinkBatchOutput.txt", true);
-			for (long str : results) {
-				writer.write((int) str);
+			for (String str : results) {
+				writer.write(str);
 			}
 			writer.close();
 		} catch (IOException e) {
--- flink/src/main/java/de/tuberlin/dima/bdapro/flink/tpch/batch/queries/Query21.java
+++ flink/src/main/java/de/tuberlin/dima/bdapro/flink/tpch/batch/queries/Query21.java
@@ -25,8 +25,8 @@ public class Query21 extends Query {
 	public List<Tuple2<String, Long>> execute(String rndNation) {
 		String querySQL = "SELECT s_name, count(*) as numwait from supplier, lineitem l1, orders, nation where s_suppkey = l1.l_suppkey "
 				+ "and o_orderkey = l1.l_orderkey and o_orderstatus = 'F' and l1.l_receiptdate > l1.l_commitdate and exists ( "
-				+ "select * from lineitem l2 where l2.l_orderkey = l1.l_orderkey and l2.l_suppkey <> l1.l_suppkey ) "
-				+ "and not exists (select * from lineitem l3 where l3.l_orderkey = l1.l_orderkey and l3.l_suppkey <> l1.l_suppkey "
+				+ "select * from lineitem l2 where l1.l_orderkey = l2.l_orderkey and l1.l_suppkey <> l2.l_suppkey ) "
+				+ "and not exists (select * from lineitem l3 where l1.l_orderkey = l3.l_orderkey and l1.l_suppkey <> l3.l_suppkey "
 				+ "and l3.l_receiptdate > l3.l_commitdate ) and s_nationkey = n_nationkey and n_name = '" + rndNation + "' group by s_name "
 				+ "order by numwait desc, s_name limit 100";
 		
