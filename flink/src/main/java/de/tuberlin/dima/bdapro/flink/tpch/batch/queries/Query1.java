7170791e2426dc272b2556bcfa25fb301f060974
Hekmatullah Sajid <hekmatullah.sajid@campus.tu-berlin.de>
2017-06-22 16:26:16 +0200

    Debugging query1

--- flink/src/main/java/de/tuberlin/dima/bdapro/flink/tpch/batch/queries/Query1.java
+++ flink/src/main/java/de/tuberlin/dima/bdapro/flink/tpch/batch/queries/Query1.java
@@ -26,17 +26,17 @@ public class Query1 extends Query {
 			final int delta) {
 		Table lineitem = env.scan("lineitem");
 
-		Table result = lineitem.where("shipdate.toDate <= ('1998-12-01'.toDate - " + delta + ".days)")
-				.groupBy("returnflag, linestatus")
-				.select("returnflag, linestatus, sum(quantity) as sum_qty, "
-						+ "sum(extendedprice) as sum_base_price, "
-						+ "sum(extendedprice*(1-discount)) as sum_disc_price, "
-						+ "sum(extendedprice*(1-discount)*(1+tax)) as sum_charge, "
-						+ "avg(quantity) as avg_qty, "
-						+ "avg(extendedprice) as avg_price, "
-						+ "avg(discount) as avg_disc, "
-						+ "count(linestatus) as count_order")
-				.orderBy("returnflag, linestatus");
+		Table result = lineitem.where("l_shipdate.toDate <= ('1998-12-01'.toDate - " + delta + ".days)")
+				.groupBy("l_returnflag, l_linestatus")
+				.select("l_returnflag, l_linestatus, sum(l_quantity) as sum_qty, "
+						+ "sum(l_extendedprice) as sum_base_price, "
+						+ "sum(l_extendedprice*(1-l_discount)) as sum_disc_price, "
+						+ "sum(l_extendedprice*(1-l_discount)*(1+l_tax)) as sum_charge, "
+						+ "avg(l_quantity) as avg_qty, "
+						+ "avg(l_extendedprice) as avg_price, "
+						+ "avg(l_discount) as avg_disc, "
+						+ "count(l_linestatus) as count_order")
+				.orderBy("l_returnflag, l_linestatus");
 
 		try {
 			return env.toDataSet(result, TypeInformation.of
